3778f3607459ed1acdea2a19a8989a473929286f
Keaton <fostygera@gmail.com>
2025-10-03 02:47:45 +0300

    added docstring for function in widget.py

--- src/widget.py
+++ src/widget.py
@@ -2,14 +2,21 @@ from src import masks
 
 
 def mask_account_card(user_data: str) -> str:
+    """Функция, которая должна получить строку, чтобы проверить, чем является строка - номером карты
+    или номером счёта. После проверки, функция обращается к модулю masks.py и возвращает маску или
+    возвращает сообщение о том, что она получила некорректные данные."""
     list_user_data = user_data.lower().replace("ё", "е").split()
     user_data_type = None
     found_index = 0
     for index, item in enumerate(list_user_data):
-        if len(item) == 16 and not "счет" in list_user_data:
+        if (
+            len(item) == 16 and not "счет" in list_user_data
+        ):  # Является ли полученная строка номером карты И НЕ счётом.
             found_index = index
             user_data_type = "card"
-        elif item == "счет" and len(list_user_data) == 2:
+        elif (
+            item == "счет" and len(list_user_data) == 2
+        ):  # Является ли строка счётом И состоит ли из 2 объектов список.
             found_index = -index - 1
             user_data_type = "account"
     if user_data_type == "card":
