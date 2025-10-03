4ca76957bafd216c90fee99d2d8f84602f1c2a82
Keaton <fostygera@gmail.com>
2025-10-03 09:23:10 +0300

    fixed function mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -5,24 +5,30 @@ def mask_account_card(user_data: str) -> str:
     """Функция, которая должна получить строку, чтобы проверить, чем является строка - номером карты
     или номером счёта. После проверки, функция обращается к модулю masks.py и возвращает маску или
     возвращает сообщение о том, что она получила некорректные данные."""
-    list_user_data = user_data.lower().replace("ё", "е").split()
+    list_formatted_user_data = user_data.lower().replace("ё", "е").split()
+    list_user_data = user_data.split()
     user_data_type = None
     found_index = 0
-    for index, item in enumerate(list_user_data):
+    for index, item in enumerate(list_formatted_user_data):
         if (
-            len(item) == 16 and not "счет" in list_user_data
+            len(item) == 16 and not "счет" in list_formatted_user_data
         ):  # Является ли полученная строка номером карты И НЕ счётом.
             found_index = index
             user_data_type = "card"
         elif (
-            item == "счет" and len(list_user_data) == 2
+            item == "счет" and len(list_formatted_user_data) == 2
         ):  # Является ли строка счётом И состоит ли из 2 объектов список.
             found_index = -index - 1
             user_data_type = "account"
     if user_data_type == "card":
-        message_for_user = f"Карта {masks.get_mask_card_number(list_user_data[found_index])}"
+        # Если строка с номером карты содержит название через пробел, код ниже изымет объект длинной 16 символов.
+        card_number = "".join([i for i in list_user_data if len(i) == 16])
+        mask_card_number = masks.get_mask_card_number(card_number)
+        # А оставшиеся объекты списка вернёт в строку как название карты.
+        card_name = " ".join([i for i in list_user_data if i != card_number])
+        message_for_user = f"{card_name} {mask_card_number}"
     elif user_data_type == "account":
-        message_for_user = f"Счёт {masks.get_mask_account(list_user_data[found_index])}"
+        message_for_user = f"Счёт {masks.get_mask_account(list_formatted_user_data[found_index])}"
     else:
         message_for_user = "Некорректный ввод: тип данных не является номером карты или номером счёта."
     return message_for_user
