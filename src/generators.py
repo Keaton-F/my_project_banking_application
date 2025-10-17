08d47cf05b89fd1016e00bcf8f55021dc01fd9db
Keaton <fostygera@gmail.com>
2025-10-17 18:27:49 +0300

    fixed 1 function and reformatted module

--- src/generators.py
+++ src/generators.py
@@ -1,6 +1,7 @@
 from typing import Dict, Generator, List
 
-def filter_by_currency(transactions: List[Dict], currency: str):
+
+def filter_by_currency(transactions: List[Dict], currency: str) -> Generator[Dict, None, None]:
     """
     Принимает список транзакций и возвращает итератор,
     который выдаёт только те, у которых код валюты совпадает с заданным.
@@ -10,15 +11,17 @@ def filter_by_currency(transactions: List[Dict], currency: str):
             yield transaction
 
 
-def transaction_descriptions(sample_transactions: List[Dict[str, str]]):
+def transaction_descriptions(sample_transactions: List[Dict[str, str]]) -> Generator[str, None, None]:
     """
     Генератор, возвращающий описание каждой транзакции из списка.
     """
     for transaction in sample_transactions:
-        yield transaction.get("description")
+        description = transaction.get("description")
+        if description is not None:
+            yield description
 
 
-def card_number_generator(start: int, end: int):
+def card_number_generator(start: int, end: int) -> Generator[str, None, None]:
     """
     Генератор, выдающий номера банковских карт
     в формате 'XXXX XXXX XXXX XXXX' от start до end включительно.
@@ -27,4 +30,4 @@ def card_number_generator(start: int, end: int):
         # Преобразуем число в строку длиной 16 символов с ведущими нулями
         formatted = f"{number:016}"
         # Разбиваем на группы по 4 символа
-        yield " ".join([formatted[i:i + 4] for i in range(0, 16, 4)])
+        yield " ".join([formatted[i : i + 4] for i in range(0, 16, 4)])
