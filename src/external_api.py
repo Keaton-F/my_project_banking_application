4143166b1a549501503b99205a06af50a71ec97e
Keaton <fostygera@gmail.com>
2025-10-26 18:55:12 +0300

    add dir data and 2 modules in src

---
+++ src/external_api.py
@@ -0,0 +1,3 @@
+def convert_to_rub(transaction: dict) -> float:
+    """Конвертирует транзакцию в рубли, используя API при необходимости."""
+    pass
---
+++ src/utils.py
@@ -0,0 +1,3 @@
+def read_json(file_path: str) -> list[dict]:
+    """Читает JSON-файл и возвращает список словарей с транзакциями."""
+    pass
